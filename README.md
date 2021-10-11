d3df7d0aac38c76e01493c1a5753c8efdd2ab89c
gonzalobaezbp <gonzalobaezbp@gmail.com>
2021-10-11 01:36:59 -0500

    server creation

---
+++ README.md
@@ -0,0 +1,2 @@
+# api-nodejs-mysql-docker
+API with Node JS, Express and MySQL (Docker deployment is optional)
---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+
+const app = express()
+
+app.set('port', 3000)
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('Welcome to my API')
+})
+
+app.listen(app.get('port'), () => {
+    console.log('Server running on port: ', app.get('port'));
+})
