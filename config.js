351848dcf9163efe8501768a15f21a2f2d4fc4f8
gonzalobaezbp <gonzalobaezbp@gmail.com>
2021-10-11 19:27:20 -0500

    update config

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 const dotenv = require('dotenv').config();
 
 module.exports = {
-  host: process.env.host || 'localhost',
-  port: process.env.port || '3306',
-  user: process.env.user || 'root',
-  password: process.env.password || 'root',
-  database: process.env.database || 'DB_TEST'
+  host: process.env.host,
+  port: process.env.port,
+  user: process.env.user,
+  password: process.env.password,
+  database: process.env.database
 }
